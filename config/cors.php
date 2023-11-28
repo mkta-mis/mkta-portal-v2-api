7964ce01bc812a369f0b91977742ad72ab70110e
mkta-mis <150634193+mkta-mis@users.noreply.github.com>
2023-11-28 14:39:18 +0800

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['*', '*.mkthemedattractions.com.ph'],
 
     'allowed_origins_patterns' => [],
 
